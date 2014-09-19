201e5f7ce7c5453a7871de50a2dc4633df1556da
dragon.metalheart@gmail.com <dragon.metalheart@gmail.com@179b3603-52ae-e132-8c2a-4f99e910d8a6>
2014-09-19 14:57:42 +0000

    -minor

---
+++ trunk/SampleWordHelper/Model/CatalogProviderInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SampleWordHelper.Model
+{
+    /// <summary>
+    /// Информация о классе, реализующем доступ к провайдеру каталога.
+    /// </summary>
+    public class CatalogProviderInfo
+    {
+        /// <summary>
+        /// Уникальный идентификатор провайдера каталога.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Экземпляр класса, реализующего провайдер каталога.
+        /// </summary>
+        public Type Class { get; private set; }
+
+        /// <summary>
+        /// Создаёт новый класса, описывающего экземпляр провайдера.
+        /// </summary>
+        public CatalogProviderInfo(string id, Type @class)
+        {
+            Id = id;
+            Class = @class;
+        }
+    }
+}
